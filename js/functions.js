94782487df17c76eb5c0c99d3c2d435aef2b5224
Boren <53597966+borenx1@users.noreply.github.com>
2021-03-12 19:04:09 +1300

    Change if else to switch statement in functions.js

--- js/functions.js
+++ js/functions.js
@@ -5,12 +5,24 @@ $(document).ready(function() {
   navbarContainer.load("common/navbar.html", function() {
     // Get the last item in the path (to be able to host on GitHub Pages)
     var path = window.location.pathname.split("/").slice(-1)[0];
-    if (path == "index.html" || path == "") {
-      $("#nav-home").addClass("active");
-    } else if (path == "table.html") {
-      $("#nav-table").addClass("active");
-    } else if (path == "quiz.html") {
-      $("#nav-quiz").addClass("active");
+    switch (path) {
+      case "index.html":
+      case "": {
+        $("#nav-home").addClass("active");
+        break;
+      }
+      case "table.html": {
+        $("#nav-table").addClass("active");
+        break;
+      }
+      case "quiz.html":
+      case "times-table-quiz.html": {
+        $("#nav-quiz").addClass("active");
+        break;
+      }
+      default: {
+        console.warn("No active navbar link for path: " + path);
+      }
     }
   });
   
